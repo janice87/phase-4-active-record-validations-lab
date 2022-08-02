7a1efa3c83861e435e8222b1d4125b0e6cf7c905
Janice Chan <janice.chan1288@gmail.com>
2022-08-02 18:50:43 -0400

    commit tree 8bdeee6bcdc62c6f8d954939a466a55df9c34a18

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :clickbait_title?
+
+    TITLES = [/Won't Believe/i, /Secret/i, /Top\d/i, /Guess/i]
+
+    def clickbait_title?
+        if TITLES.none? do |t|
+            t.match title
+        end
+            errors.add(:title, "You need a clickbait title!")
+        end
+    end
+    
 end
+
